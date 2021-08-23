e24ba25c01118cd2756a926159d04624d521e211
cremc <cremcircle@gmail.com>
2021-08-23 22:09:47 +0530

    Add Course Domain Model

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -9,9 +9,24 @@ namespace CleanArch.Domain.Models
     public class Course
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        //public string Name { get; set; }
+        //public string Description { get; set; }
+        public int InstitutionID { get; set; }
+        public int CASL1SubinstitutionID { get; set; }
+        public int CASL2SubinstitutionID { get; set; }
+        public int CASL3SubinstitutionID { get; set; }
+        public int CASL4SubinstitutionID { get; set; }
+        public string CourseName { get; set; }
+        public string CourseStartSession { get; set; }
+        public int CourseDurationDays { get; set; }
+        public int CourseDurationWeeks { get; set; }
+        public int CourseDurationMonths { get; set; }
+        public int CourseDurationAcademicSessions { get; set; }
+        public string CourseDescriptionForCareer { get; set; }
+        public string CourseDescriptionForRigour { get; set; }
+        public int TargetStudentTypeLookupID { get; set; }
+        public int AttendanceTypeLookupID { get; set; }
+
     }
 
 }
