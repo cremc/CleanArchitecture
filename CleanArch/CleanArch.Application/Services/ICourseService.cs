70ac626142d4831d49e37ba0b6542f5b0d225683
cremc <cremcircle@gmail.com>
2021-08-27 01:03:45 +0530

    Add CourseViewModel, Interfaces for CourseService (in Application layer) and CourseRepository in (Domain layer)

---
+++ CleanArch/CleanArch.Application/Services/ICourseService.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public interface ICourseService
+    {
+        public IEnumerable<CourseViewModel> GetCourses();
+
+    }
+}
