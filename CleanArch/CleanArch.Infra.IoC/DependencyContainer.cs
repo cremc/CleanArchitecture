7a1ab10f687dec9ddd0af0a960d3409298bc4eb8
cremc <cremcircle@gmail.com>
2021-08-29 01:05:03 +0530

    Add DependencyContainer, Configure Presentation layer for DependencyContainer

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application layer
+            services.AddScoped<ICourseService, CourseService>();
+
+
+            //Infrastructure.Data layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
