ba97f80ecffff77df71856248e9df2269c1ece42
Atul <bytalforce@gmail.com>
2021-09-17 18:25:41 +0530

    Add new CleanArch.Api project

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch.Application.Services;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+        private ICourseService _courseService;
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public CourseViewModel Get()
+        {
+            CourseViewModel courseViewModel = _courseService.GetCourses();
+            return courseViewModel;
+        }
+    }
+}
